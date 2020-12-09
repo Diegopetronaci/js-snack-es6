63e8eef90c324e8c7985fb1e73dbce06f91fb2fe
Diego <Petronacidiego@gmail.com>
2020-12-09 15:04:20 +0100

    commit iniziale

---
+++ sneak/esercizio 1/main.js
@@ -0,0 +1,14 @@
+//L’utente inserisce due numeri in successione, con due
+//prompt.Il software stampa il maggiore.
+
+const primo = 2;
+const secondo = 5;
+
+const maggiore = [];
+
+const cercaMaggiore = (`${primo < secondo ? maggiore.push(secondo) : maggiore.push(primo)}`);
+/* console.log(cercaMaggiore); */
+
+/* console.log(`${userName} is ${(now - yob) < 40 ? "still youngh" : "getting old"} `); */
+
+console.log(maggiore);
